8f3ae439e05786e114a6d70ba413d981be60299d
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-07-31 11:29:26 +0700

    Export Riot history provider

--- packages/adapter-lol/src/index.ts
+++ packages/adapter-lol/src/index.ts
@@ -1,6 +1,7 @@
 export * from './adapter.ts';
 export * from './provider.ts';
 export * from './riot-context-provider.ts';
+export * from './riot-history-provider.ts';
 export * from './riot-provider.ts';
 export * from './riot-resolved-provider.ts';
 export * from './types.ts';

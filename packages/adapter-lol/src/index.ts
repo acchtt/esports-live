d721fe4f34a00a3e12b1b4f84751a8f1dbb1d4a4
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-08-04 20:29:10 +0700

    Export delayed Riot live recovery provider

--- packages/adapter-lol/src/index.ts
+++ packages/adapter-lol/src/index.ts
@@ -4,6 +4,7 @@ export * from './provider.ts';
 export * from './riot-consistent-provider.ts';
 export * from './riot-context-provider.ts';
 export * from './riot-current-player-provider.ts';
+export * from './riot-delayed-live-recovery-provider.ts';
 export * from './riot-history-provider.ts';
 export * from './riot-provider.ts';
 export * from './riot-resolved-provider.ts';

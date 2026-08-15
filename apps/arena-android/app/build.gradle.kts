61e2b1be44e94fb1e95d294fcd3c16b17b185e32
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-08-15 10:24:01 +0700

    Use Kotlin compiler options DSL

--- apps/arena-android/app/build.gradle.kts
+++ apps/arena-android/app/build.gradle.kts
@@ -1,4 +1,5 @@
 import java.io.File
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
@@ -77,12 +78,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
